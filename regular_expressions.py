e07b5b3df95c4df93306d9fbe02f4432c49c344d
Ilya Erlingas <Aligruit@gmail.com>
2020-02-11 20:34:24 +0300

    #FINAL VERSION#

--- regular_expressions.py
+++ regular_expressions.py
@@ -5,25 +5,39 @@ with open("phonebook_raw.csv", encoding='utf8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 result = []
-for text in contacts_list:
-    pattern_phone = re.compile(r'\+7|8\s|(495)|[\s]\s|(913|983|748)\D*(\d+)\D*(\d{2})|\s(\d{4})')
-    pattern_emails = re.compile(r'(\w*@\w*\.ru)')
+for text in contacts_list:  # list with all stings from original file!!!
+    # formating one by one
+    contact = []
+    fio = ' '.join(text[0:2])  # getting lastname, surname and firstname in a single string(first three fields)
+    employer_fio = re.findall(r'([А-Яа-я]+)', fio)  # finding all the words in russian language by findall
+    while len(employer_fio) < 3:  # if fields lastname, surname and firstname < then 3 then we put the mark '-' in there
+        employer_fio.append('-')
+    contact += employer_fio
+    # then we deal with some other field, for example organization:
     pattern_organization = re.compile(r'(Минфин|ФНС)')
-    pattern_eployers = re.compile(r'(^[А-Яа-я]+).([А-Яа-я]+).([А-Яа-я]+)')
+    text_organization = pattern_organization.sub(r'\1', text[3])
+    contact.append(text_organization)
+    # after finding the position:
     pattern_position = re.compile(
         r'(главный специалист – эксперт отдела взаимодействия с федеральными органами власти Управления '
         r'налогообложения имущества и доходов физических лиц|cоветник отдела Интернет проектов Управления '
-        r'информационных технологий)')
-    text_phone = pattern_phone.sub(r'+7 (\1)\2-\3-\4 доб.\5', str(text))
-    text_last_name = pattern_eployers.sub(r'\1' r'\2' r'\3', str(text))
-    text_organization = pattern_organization.sub(r'\1', str(text))
-    text_emails = pattern_emails.sub(r'1', str(text))
-    text_position = pattern_position.sub(r'\1', str(text))
-    result = [text_phone, text_last_name, text_organization, text_emails, text_position]
-cleared_contacts = []
-with open('phonebook.csv', 'a') as csv_file:
+        r'информационных технологий)'
+    )
+    text_position = pattern_position.sub(r'\1', text[4])
+    contact.append(text_position)
+    pattern_phone = re.compile(r"(\+7|7|8)[\s\(]*(\d+)[\)\s]*(\d+)-?(\d+)-?(\d+)\s?(\(?доб.\s\d{1,4}\)?)?")
+    # better to compile the regex on regex101.compile
+    pattern_emails = re.compile(r'([^@]@[^@].ru)')  # compiling the e-mails
+    text_emails = pattern_emails.sub(r'1', text[6])
+    contact.append(text_emails)
+    print(text_emails)
+    result.append(contact)
+# saving the all the regexed strings in regex and writting in new csv file
+with open('phonebook.csv', 'w') as csv_file:
     the_writer = csv.writer(csv_file, delimiter=',')
     q = ['lastname', 'firstname', 'surname', 'organization', 'position', 'phone', 'email']
     the_writer.writerow(q)
     for row in result:
-        the_writer.writerow(result[1].split(','), result[2], result[4], result[0], result[3])
+        the_writer.writerow(row)
+        
+# so the job is done
